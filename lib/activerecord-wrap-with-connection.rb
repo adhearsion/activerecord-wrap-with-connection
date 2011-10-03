2bec6907aaef94fe3058047c7c990630969a6f13
Ben Langfeld <ben@langfeld.me>
2011-10-03 12:04:46 +0100

    Dump some code to begin with - not sure if this is fully up to date with Rails 3 but it's close

--- lib/activerecord-wrap-with-connection.rb
+++ lib/activerecord-wrap-with-connection.rb
@@ -1,5 +1,30 @@
 require "activerecord-wrap-with-connection/version"
 
-module ActiverecordWrapWithConnection
-  # Your code goes here...
+methods_to_wrap = {
+  (class<<ActiveRecord::Base;self;end) => [
+    :find, :first, :last, :all, :destroy, :destroy_all, :exists?, :delete, :delete_all, :update, :update_all, :find_each, :find_in_batches, :select, :group, :order, :reorder, :limit, :joins, :where, :preload, :eager_load, :includes, :from, :lock, :readonly, :having, :create_with, :count, :average, :minimum, :maximum, :sum, :calculate, :find_by_sql, :count_by_sql, :quoted_table_name, :reset_sequence_name, :table_exists?, :columns, :quote_value, :sanitize, :quote_bound_value, :encode_quoted_value
+  ],
+  ActiveRecord::Base => [:create, :, :quote_value],
+  ActiveRecord::Associations::AssociationCollection => [:initialize, :find, :find_target, :load_target, :count, :group, :order, :limit, :joins, :where, :preload, :eager_load, :includes, :from, :lock, :readonly, :having],
+  ActiveRecord::Associations::HasAndBelongsToManyAssociation => [:insert_record, :delete_records, :],
+  ActiveRecord::Associations::HasManyThroughAssociation => [:construct_conditions],
+  ActiveRecord::Associations::HasOneAssociation => [:construct_sql],
+  ActiveRecord::Associations::ClassMethods => [:collection_reader_method, :configure_dependency_for_has_many],
+  ActiveRecord::Calculations::ClassMethods => [:calculate],
+}
+
+methods_to_wrap.each do |klass, methods|
+  methods.each do |method|
+    klass.class_eval do
+      alias_method_chain method, :connection do |target, punc|
+        eval %{
+          def #{target}_with_connection#{punc}(*a, &b)
+            ActiveRecord::Base.connection_pool.with_connection do
+              #{target}_without_connection#{punc}(*a, &b)
+            end
+          end
+        }
+      end
+    end
+  end
 end
